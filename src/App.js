3fe64ffda08a79e98f3f0b67ae2f7cd7888852c6
Boyan Velikov <boyanvelikov@gmail.com>
2023-01-09 07:45:07 +0200

    rendering

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 
+
+
+const Person = () => {
+  return (
+    <>
+    <h1>Name: John</h1>
+    <h2>Last Name: Doe</h2>
+    <h2>Age: 30</h2>
+    </>
+  )
+};
+
 const App = () => {
-  const name = 'Jane';
+  // const name = 'Jane';
   // const isNameShowing = true;
 
   return (
     <div className="App">
       <h1>Hello, {2 + 2}</h1>
-      {name ? (
+      {/* {name ? (
         <>
         <h1>{name}</h1>
         </>
@@ -17,7 +29,7 @@ const App = () => {
         <h1>test</h1>
         <h2>There is no name</h2>
         </>
-      )}
+      )} */}
     </div>
   );
 }
