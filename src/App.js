f88e6b2b6cb85913e66299c50fa4e8eede874435
Boyan Velikov <boyanvelikov@gmail.com>
2023-01-15 09:22:29 +0200

    start state

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 
 
 
-const Person = (props) => {
-  return (
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: {props.LastName}</h2>
-    <h2>Age: {props.age}</h2>
-    </>
-  )
-};
+// const Person = (props) => {
+//   return (
+//     <>
+//     <h1>Name: {props.name}</h1>
+//     <h2>Last Name: {props.LastName}</h2>
+//     <h2>Age: {props.age}</h2>
+//     </>
+//   )
+// };
 
 const App = () => {
   // const name = 'Jane';
@@ -18,8 +18,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <Person name='John' LastName='Doe' age={25}/>
-      <Person name='Marry' LastName='Doe' age={25}/>
+      {/* <Person name='John' LastName='Doe' age={25}/>
+      <Person name='Marry' LastName='Doe' age={25}/> */}
       {/* <Person/>
       <Person/>
       <Person/> */}
