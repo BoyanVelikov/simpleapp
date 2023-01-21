a223bffd50d697a32cc1fe38af3fcef8f984e520
Boyan Velikov <boyanvelikov@gmail.com>
2023-01-21 11:17:21 +0200

    clicked

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <button>-</button>
+      <button onClick={() => alert('clicked')}>-</button>
       <h1>{counter}</h1>
       <button>+</button>
       {/* <Person name='John' LastName='Doe' age={25}/>
