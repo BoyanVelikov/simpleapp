47f62e26b0c792f33238529cc2c0983a6e22b6d3
Boyan Velikov <boyanvelikov@gmail.com>
2023-01-28 10:38:13 +0200

    data fetch

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ const API_URL = 'http://www.omdpapi.com?apikey=c032e2d7';
 const App = () => {
 
   const searchMovies = async (title) => {
-    const response = fetch(`${API_URL}&s=${title}`);
-  }
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  };
 
   useEffect(() => {
-
+    searchMovies('Spiderman');
   },[]);
 
   return (
